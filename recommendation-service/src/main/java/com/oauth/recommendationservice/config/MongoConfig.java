45325f04e196b1e5de973c9bb48c0d850450a7ac
mrahul2001 <mitrarahul2001@gmail.com>
2026-05-24 20:27:22 +0530

    updating

--- recommendation-service/src/main/java/com/oauth/recommendationservice/config/MongoConfig.java
+++ recommendation-service/src/main/java/com/oauth/recommendationservice/config/MongoConfig.java
@@ -1,9 +1,20 @@
 package com.oauth.recommendationservice.config;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
+    @Value("${spring.mongodb.uri}")
+    private String mongoUri;
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
 }
